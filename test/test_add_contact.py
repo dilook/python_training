147fdf3769393626503c9dc493085a375256686a
dilook <bss4work@yandex.ru>
2020-07-21 09:33:03 +0300

    ref: extract contact helper class

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,12 +13,12 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     new_contact = Contact(first_name="Dia", middle_name="Korret", last_name="Medercy", nick="dia",
                           day="1", month="April", year="1978",
                           work_phone="45-49-45", mobile_phone="+45135464151", email="test@test.com",
                           company="Google", address="25 Korovina str", homepage="humster.com",
                           group="[none]", title="Dia"
                           )
-    app.add_new_contact(new_contact)
-    app.logout()
+    app.contact.add_new(new_contact)
+    app.session.logout()
