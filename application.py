f0b19466dbfaec5e46e7368c6f6aae1e6795f400
dilook <bss4work@yandex.ru>
2020-07-21 09:06:41 +0300

    ref: move contact test's to pytest

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import configparser
 
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 from webdriver_manager.firefox import GeckoDriverManager
 
 
@@ -37,15 +38,53 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, username, password):
+    def open_home_page(self):
         wd = self.wd
         wd.get(self.base_url)
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
+    def add_new_contact(self, contact):
+        wd = self.wd
+
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nick)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work_phone)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.day)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.month)
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.year)
+        Select(wd.find_element_by_name("new_group")).select_by_visible_text(contact.group)
+        wd.find_element_by_css_selector("[value=Enter]").click()
+
+        self.open_home_page()
+
     def destroy(self):
         self.wd.quit()
-
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,101 +1,24 @@
 # -*- coding: utf-8 -*-
-import configparser
-import unittest
+import pytest
 
-from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import Select
-from webdriver_manager.firefox import GeckoDriverManager
-
+from application import Application
 from contact import Contact
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        config = configparser.ConfigParser()
-        config.read("setup.ini")
-        self.base_url = config['DEFAULT']['url']
-        self.wd = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-        self.wd.implicitly_wait(5)
-
-    def test_add_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        new_contact = Contact(first_name="Dia", middle_name="Korret", last_name="Medercy", nick="dia",
-                              day="1", month="April", year="1978",
-                              work_phone="45-49-45", mobile_phone="+45135464151", email="test@test.com",
-                              company="Google", address="25 Korovina str", homepage="humster.com",
-                              group="[none]", title="Dia"
-                              )
-        self.add_new_contact(wd, new_contact)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def add_new_contact(self, wd, contact):
-        wd.find_element_by_link_text("add new").click()
-
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nick)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_phone)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.day)
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.month)
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.year)
-        Select(wd.find_element_by_name("new_group")).select_by_visible_text(contact.group)
-        wd.find_element_by_css_selector("[value=Enter]").click()
-
-        self.open_home_page(wd)
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get(self.base_url)
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    new_contact = Contact(first_name="Dia", middle_name="Korret", last_name="Medercy", nick="dia",
+                          day="1", month="April", year="1978",
+                          work_phone="45-49-45", mobile_phone="+45135464151", email="test@test.com",
+                          company="Google", address="25 Korovina str", homepage="humster.com",
+                          group="[none]", title="Dia"
+                          )
+    app.add_new_contact(new_contact)
+    app.logout()
