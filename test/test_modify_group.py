9f56ae0e54ea63eccc8f77af6913d63c874ed178
dilook <bss4work@yandex.ru>
2020-07-23 11:59:27 +0300

    ref: change name of test to correct

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,5 +3,5 @@ from model.group import Group
 
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_group(Group(name="MODIFIED"))
+    app.group.modify_first_group(Group(name="MODIFIED"))
     app.session.logout()
