ae2dfd93186f38f308f038a8e89ec943f8fc7d07
dilook <bss4work@yandex.ru>
2020-08-06 00:40:24 +0400

    ref: using empty Group object

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,6 +12,6 @@ def test_add_group(app):
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create(Group())
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
