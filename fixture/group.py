7a4d69871cc22c78ec6d044849ac0a40b2d87f53
dilook <bss4work@yandex.ru>
2020-08-06 01:10:30 +0400

    ref: fill_group_form method

--- fixture/group.py
+++ fixture/group.py
@@ -10,9 +10,7 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        self.set_field("group_name", group.name)
-        self.set_field("group_footer", group.footer)
-        self.set_field("group_header", group.header)
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
@@ -36,11 +34,14 @@ class GroupHelper:
         self.open_groups_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
+        self.fill_group_form(group)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
         self.set_field("group_name", group.name)
         self.set_field("group_footer", group.footer)
         self.set_field("group_header", group.header)
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
 
     def set_field(self, name, value):
         wd = self.app.wd
