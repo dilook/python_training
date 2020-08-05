3846095e957afe5dff5209fd965d6eebd63c29eb
dilook <bss4work@yandex.ru>
2020-08-06 00:22:22 +0400

    add: list asserts in group tests

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,4 +4,7 @@ from model.group import Group
 def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Name_of_group"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="MODIFIED"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
