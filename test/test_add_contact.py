c486664f128709ced7c852b1bb98181be08f74ba
dilook <bss4work@yandex.ru>
2020-08-06 12:11:35 +0400

    add: hashing to contact tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,8 +10,8 @@ def test_add_contact(app):
                           homepage="humster.com", group="[none]", title="Dia")
     old_contacts = app.contact.get_contact_list()
     app.contact.add_new(new_contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(new_contact)
     assert sorted(old_contacts, key=Contact.get_id_or_max) == sorted(new_contacts, key=Contact.get_id_or_max)
 
@@ -20,7 +20,7 @@ def test_add_empty_contact(app):
     old_contacts = app.contact.get_contact_list()
     new_contact = Contact(first_name="", last_name="")
     app.contact.add_new(new_contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(new_contact)
     assert sorted(old_contacts, key=Contact.get_id_or_max) == sorted(new_contacts, key=Contact.get_id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -8,7 +8,7 @@ def test_modify_first_contact(app):
     contact = Contact(first_name="MODIFIED", last_name="MIMI")
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.get_id_or_max) == sorted(new_contacts, key=Contact.get_id_or_max)
