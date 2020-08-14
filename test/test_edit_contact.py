69ed828a878adee8b0ddb8719917d22705121bbd
dilook <bss4work@yandex.ru>
2020-08-14 16:18:54 +0400

    rem: print for debug

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -8,7 +8,6 @@ from fixture.string_utils import prepare_link
 def test_contact_on_edit_form(app):
     contacts_count = app.contact.count()
     index = randrange(contacts_count)
-    print(index)
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones == merge_phones_like_on_homepage(contact_from_edit_page)
