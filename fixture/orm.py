4ee0a17d0ee61df22dd31924c83a2bfba8a0bc5b
dilook <bss4work@yandex.ru>
2020-08-28 15:53:04 +0300

    add: orm relations

--- fixture/orm.py
+++ fixture/orm.py
@@ -6,6 +6,7 @@ from model.contact import Contact
 from model.group import Group
 
 
+# noinspection PyTypeChecker
 class ORMFixture:
     db = Database()
 
@@ -15,6 +16,8 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups",
+                       lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -22,6 +25,8 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts",
+                     lazy=True)
 
     def __init__(self, host, port, name, user, password):
         self.db.bind("mysql", host=host, port=port, database=name, user=user, password=password)
@@ -46,3 +51,14 @@ class ORMFixture:
             return Contact(id=str(contact.id), first_name=contact.firstname, last_name=contact.lastname)
 
         return list(map(convert, contacts))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
--- test/check_db_connection.py
+++ test/check_db_connection.py
@@ -1,8 +1,9 @@
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="80.211.17.161", port=3306, name="addressbook", user="root", password="Too_Borg_23")
 
-l = db.get_contact_list()
+l = db.get_contacts_not_in_group(Group(id=200))
 for item in l:
-    print(l)
+    print(item)
 print(len(l))
