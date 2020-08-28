b474c8d012d260c85556106d1d578f64cace615b
dilook <bss4work@yandex.ru>
2020-08-28 14:04:54 +0300

    add: test to match contacts from ui to db

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,13 +1,15 @@
-import re
-
+from model.contact import Contact
 from model.group import Group
+from utils.db_utils import clean_group_name, clean_contact_name
 
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
-
-    def clean(group):
-        return Group(id=group.id, name=re.sub(' +', ' ', group.name.strip()))
-
-    db_list = map(clean, db.get_group_list())
+    db_list = map(clean_group_name, db.get_group_list())
     assert sorted(ui_list, key=Group.get_id_or_max) == sorted(db_list, key=Group.get_id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    db_list = map(clean_contact_name, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.get_id_or_max) == sorted(db_list, key=Contact.get_id_or_max)
